f0f62635c929d66b5e4eca98063f3b66b624cd27
Tomas Brukner <xbrukner@fi.muni.cz>
2013-11-28 22:09:25 +0100

    Added Catch framework for unit tests.

--- main.cpp
+++ main.cpp
@@ -86,6 +86,22 @@ Cl_Session<T>::Cl_Session() :
              )
 {}
 
+#if 0
+#define CATCH_CONFIG_MAIN
+#include "Catch/single_include/catch.hpp"
+
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
+#endif
+
 
 int main()
 {
@@ -102,4 +118,3 @@ int main()
     return 0;
 
 }
-
