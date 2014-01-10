10ebd3ebaa8b6b9644a650c848768ef087b61909
Tomas Brukner <xbrukner@fi.muni.cz>
2014-01-10 22:25:39 +0100

    Changed the MAIN macro - content extracted to kontr::main.

--- kontr.h
+++ kontr.h
@@ -6,27 +6,39 @@
 
 #include "kontr-internal.h"
 
+namespace kontr {
+    /// Main for the MAIN macro.
+    void main(
+            Names::Configuration::SessionDelegator::Function NameSession,
+             Generator::Configuration::SessionDelegator::Function GeneratorSession
+            ) {
+        //Both parameters must have explicit type, so the type inference works
+
+        using namespace std;
+        using namespace kontr;
+        cout << "Kontr Tests Generator & Verificator" << endl << endl;
+        Generator::Configuration &cg = Generator::Configuration::instance();
+
+        cout << "Getting names of tests... ";
+        cg.storage.names = Names::getAll(NameSession, cg);
+        cout << "done!" << endl;
+
+        cg.storage.nextFileName = cg.storage.names.session.c_str();
+
+        cout << "Generating... ";
+        cg.setSession(GeneratorSession);
+        cg.session->pre_test();
+        cg.session->post_test();
+        cout << "done!" << endl;
+
+        cout << "Output scripts directory: " << cg.session->__getScriptsDir() << endl;
+        exit(0); //Since ::main does not return anything
+    }
+}
+
 #define MAIN \
 int main(void) { \
-    using namespace std; \
-    using namespace kontr; \
-    cout << "Kontr Tests Generator & Verificator" << endl << endl; \
-\
-    Generator::Configuration &cg = Generator::Configuration::instance(); \
-\
-    cout << "Getting names of tests... "; \
-    cg.storage.names = Names::getAll(::Session, cg); \
-    cout << "done!" << endl; \
-\
-    cg.storage.nextFileName = cg.storage.names.session.c_str(); \
-\
-    cout << "Generating... "; \
-    cg.setSession(::Session); \
-    cg.session->pre_test(); \
-    cg.session->post_test(); \
-    cout << "done!" << endl; \
-\
-    cout << "Output scripts directory: " << cg.session->__getScriptsDir() << endl; \
+    ::kontr::main(::Session, ::Session); \
 }
 
 //Has to be the last one
